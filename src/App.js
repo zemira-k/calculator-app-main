03c72c6a0fa948784ca72f3b0016be9c6be76649
zemira kehati <kehati10@gmail.com>
2022-11-21 05:52:43 +0200

    first styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  const buttonsArr = [
+    7,
+    8,
+    9,
+    "DEL",
+    4,
+    5,
+    6,
+    "+",
+    1,
+    2,
+    3,
+    "-",
+    ".",
+    0,
+    "/",
+    "x",
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="calculator-header">
+          <div>
+            <p>calc</p>
+          </div>
+          <div className="calculator-theme">
+            <div>THEME</div>
+            <div className="calculator-theme-info">
+              <div className="calculator-theme-num">1 2 3</div>
+              <div className="calculator-btns">
+                <button className="calculator-btn"></button>
+                <button className="calculator-btn"></button>
+                <button className="calculator-btn"></button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="calculator-sum">399,981</div>
+        <div className="all-buttons-container">
+          <div className="buttons-container">
+            {buttonsArr.map((btn) => {
+              return (
+                <button className="btn small-btn" value={btn}>
+                  {btn}
+                </button>
+              );
+            })}
+          </div>
+          <div className="buttom-btns">
+            <button className="btn big-btn" value="RESET">
+              RESET
+            </button>
+            <button className="btn big-btn">=</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
